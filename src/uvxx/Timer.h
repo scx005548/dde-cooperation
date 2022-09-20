cc01fb778e3829d733e8b804a392ce7af7f51c31
zsien <quezhiyong@uniontech.com>
2022-09-20 14:18:37 +0800

    feat: uvxx 增加 timer
    uvxx 增加 timer
    
    Log:
    Influence: 无
    Task: https://pms.uniontech.com/task-view-119615.html
    Change-Id: I8a436be5a0302fc6f7631da452866e6f40ec0ef7

---
+++ src/uvxx/Timer.h
@@ -0,0 +1,29 @@
+#ifndef UVXX_TIMER_H
+#define UVXX_TIMER_H
+
+#include "Handle.h"
+
+namespace uvxx {
+
+class Timer : public HandleT<uv_timer_t> {
+public:
+    explicit Timer(const std::shared_ptr<Loop> &loop, const std::function<void()> &cb);
+
+    bool start(uint64_t milliseconds);
+    bool oneshot(uint64_t milliseconds);
+    bool stop();
+
+    bool reset();
+
+private:
+    bool isOneShot_;
+    uint64_t timeout_;
+    std::function<void()> cb_{nullFunc{}};
+
+    bool startAux();
+    void timerCb(uv_timer_t *handle);
+};
+
+} // namespace uvxx
+
+#endif // !UVXX_TIMER_H
