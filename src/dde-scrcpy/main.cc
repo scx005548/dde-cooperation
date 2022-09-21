b0c82be21f2fdacfbec87cd5be2ae82d0c72abd2
zsien <quezhiyong@uniontech.com>
2022-09-21 20:51:50 +0800

    feat: 安卓设备协同功能
    安卓设备协同功能
    
    Log: 增加安卓设备协同功能
    Influence: 无
    Task: https://pms.uniontech.com/task-view-119615.html
    Change-Id: I5bbab62a2aa61aa3e7b9786e1f9d8d7dbcf70176

--- src/dde-scrcpy/main.cc
+++ src/dde-scrcpy/main.cc
@@ -1,11 +1,16 @@
 #include <QtGui/QGuiApplication>
 
+#include "MainWindow.h"
+
 int main(int argc, char *argv[]) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
     QGuiApplication app(argc, argv);
+
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
