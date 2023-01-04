9fdf7fca65fa75157ac123bb06f2e14bdcb5f783
zhaoyingzhen <zhaoyingzhen@uniontech.com>
2023-01-04 17:24:33 +0800

    fix: 解决设备共享时关闭网卡后输入设备未释放的问题。
    修改keep-alive为10s判断网络断开。设置tcp在10s内未收到对方的ack,则认为超时。
    
    Log:
    Bug: https://pms.uniontech.com/bug-view-181371.html
    Influence: 网络断开后释放输入设备grab.
    Change-Id: I3c24d8b2ea59d3a6c2fdea7083646303bdf6faf6

--- src/utils/net.h
+++ src/utils/net.h
@@ -21,30 +21,36 @@ inline std::string getHostname(void) {
 inline void tcpSocketSetKeepAliveOption(int fd) {
     //开启保活
     int enable = 1;
-    if(setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE,
+    if (setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE,
                    &enable, sizeof(enable)) != 0) {
         qWarning("fail to enable SO_KEEPALIVE");
     }
 
-    //失连时间(30,5,5)
-    int idle = 30;
-    if(setsockopt(fd, SOL_TCP, TCP_KEEPIDLE,
+    //失连时间
+    int idle = 11;
+    if (setsockopt(fd, SOL_TCP, TCP_KEEPIDLE,
                    &idle, sizeof(idle)) != 0) {
         qWarning("fail to enable TCP_KEEPIDLE");
     }
 
     //重连时间
-    int retryInterval = 5;
-    if(setsockopt(fd, SOL_TCP, TCP_KEEPINTVL,
+    int retryInterval = 2;
+    if (setsockopt(fd, SOL_TCP, TCP_KEEPINTVL,
                    &retryInterval, sizeof(retryInterval)) != 0) {
         qWarning("fail to enable TCP_KEEPINTVL");
     }
 
     //重连次数
     int retryCnt = 5;
-    if(setsockopt(fd, SOL_TCP, TCP_KEEPCNT,
+    if (setsockopt(fd, SOL_TCP, TCP_KEEPCNT,
                    &retryCnt, sizeof(retryCnt)) != 0) {
         qWarning("fail to enable TCP_KEEPCNT");
+    }
+
+    // 10s内收不到ack即超时处理
+    unsigned int timeout = 10000;
+    if (setsockopt(fd, IPPROTO_TCP, TCP_USER_TIMEOUT, &timeout, sizeof(timeout))) {
+        qWarning("set TCP_USER_TIMEOUT option error");
     }
 }
 
