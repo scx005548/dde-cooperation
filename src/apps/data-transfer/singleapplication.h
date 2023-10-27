07e85e0cdde9028471de58e20fe837f0a8e8b0c9
liujinchang <liujinchang@uniontech.com>
2023-10-27 09:55:38 +0800

    feat: [dapplication] add dapplication for uos
    add dapplication for uos
    
    Log: add dapplication for uos

---
+++ src/apps/data-transfer/singleapplication.h
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef SINGLEAPPLICATION_H
+#define SINGLEAPPLICATION_H
+
+#include <QApplication>
+#include "type_defines.h"
+
+QT_BEGIN_NAMESPACE
+class QLocalServer;
+QT_END_NAMESPACE
+
+namespace deepin_cross {
+class SingleApplication : public CrossApplication
+{
+    Q_OBJECT
+public:
+    explicit SingleApplication(int &argc, char **argv, int = ApplicationFlags);
+    ~SingleApplication();
+    bool setSingleInstance(const QString &key);
+    void closeServer();
+
+Q_SIGNALS:
+    void raiseWindow();
+
+protected:
+    void initConnect();
+
+protected Q_SLOTS:
+    void handleNewConnection();
+    QString userServerName(const QString &key);
+
+private:
+    QLocalServer *localServer { nullptr };
+};
+}
+
+#endif   // SINGLEAPPLICATION_H
---
+++ src/apps/data-transfer/type_defines.h
@@ -0,0 +1,12 @@
+﻿#ifndef TYPE_DEFINES_H
+#define TYPE_DEFINES_H
+
+#ifdef WIN32
+#    include <QApplication>
+typedef QApplication CrossApplication;
+#else
+#    include <DApplication>
+typedef DTK_WIDGET_NAMESPACE::DApplication CrossApplication;
+#endif
+
+#endif   // TYPE_DEFINES_H
