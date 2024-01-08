debbab31931fc0f214ffa7b87e71a0f94d6e3f48
max.lv <lvwujun@uniontech.com>
2024-01-08 15:26:38 +0800

    doc: add languages setting
    to adapt the languages
    
    Log: to adapt the languages
    Bug: no

---
+++ translations/deepin-data-transfer-desktop/desktop_zh_CN.ts
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN"><context><name>desktop</name><message><location filename="Desktop Entry]GenericName" line="0"/><source>Data Transfer</source><translation>数据迁移</translation></message><message><location filename="Desktop Entry]Comment" line="0"/><source>Data transfer is a powerful tool that helps you transfer your files, settings from windows to linux.</source><translation>数据迁移是一款强大的工具，可帮助您将windows的文件、设置等迁移到Linux系统。</translation></message><message><location filename="Desktop Entry]Name" line="0"/><source>Deepin Data Transfer</source><translation>深度数据迁移</translation></message></context></TS>
